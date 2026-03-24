254a27b0614089eeddca309ed1145c5ed5faa68f
Ivanvelandia1608 <ivelandia@seidoranalytics.com>
2026-03-24 19:37:44 +0000

    Changes logic app

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,13 @@ if ingredients_list:
     #st.write(ingredients_string)
 
 
-    my_insert_stmt = """INSERT INTO smoothies.public.orders (ingredients, name) VALUES (?, ?)"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + """','"""+name_on_order + """')"""
      
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        session.sql(my_insert_stmt, (ingredients_string, name_on_order)).collect()
+        session.sql(my_insert_stmt).collect()
         
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
